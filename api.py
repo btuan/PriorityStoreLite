0717736f7bde3b0392ac56a4f9807de18e77f887
Malika Aubakirova <aubakirova.m.m@gmail.com>
2017-05-21 19:38:55 -0500

    No more deadling with blocks

--- api.py
+++ api.py
@@ -12,8 +12,6 @@ from random import randrange
 from subprocess import run
 import time
 import numpy as np
-from multiprocessing import Process
-
 
 class PriorityStoreLite:
     def __init__(self, config_dir):
@@ -60,42 +58,20 @@ class PriorityStoreLite:
     def execute_command(self, node, command):
         return run(['ssh', node, command])
 
-    def execute_scp(self, node, filename, output='./'):
-        return run(['scp', node + ':' + self.config['path'] + filename, output])
-
-    def execute_create(self, node, basename, command):
-        self.execute_command(node, 'mkdir -p "{}"'.format(self.config['path'] + basename))
-        self.execute_command(node, command)
-
-    def execute_on_nodes(self, nodes, func, args):
-        ps = []
-        # spawn threads
-        for i in nodes:
-            node = self.datanodes[i]; tmp_args = (node,) + args
-            p = Process(target=func, args=tmp_args)
-            p.start(); ps.append(p)
-        # join all the threads
-        for i in nodes:
-            ps[i].join()
-
-    def create_file(self, filename, size=67108864*2, node=None, priority=0):
-        if filename in self.metadata:
-            return None
-        
-        if node is None:
-            node = self.datanodes[randrange(len(self.datanodes))]
-        elif node not in self.datanodes:
+    def create_file(self, filename, size=67108864, node=None, priority=0):
+        if filename in self.metadata or (node is not None and node not in self.datanodes):
             return None
 
         path = self.config['path'] + filename
-        num_blocks = int(np.ceil(size*1.0/self.block_size))
-        command = "head -c {} </dev/urandom > {}".format(self.block_size, self.config['path'] + filename)
-        nodes = self.placement_node_ids(num_blocks)
-        if nodes is None:
+        command = "head -c {} </dev/urandom > {}".format(size, self.config['path'] + filename)
+        node_id = self.placement_node_ids()
+        if node_id is None:
             return None
+        node = self.datanodes[node_id]
 
         self.metadata[filename] = {
-            'nodes': nodes,
+            'node_id': node_id,
+            'node': self.datanodes[node_id],
             'modified': time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime()),
             'priority': priority,
         }
@@ -106,45 +82,43 @@ class PriorityStoreLite:
         else:
             basename = ''
 
-        self.execute_on_nodes(nodes, self.execute_create, args=(basename, command))
+        self.execute_command(node, 'mkdir -p "{}"'.format(self.config['path'] + basename))
+        self.execute_command(node, command)
         return 1.0
 
-    def placement_node_ids(self, num_blocks=3):
-        nodes = []
-        for i in range(num_blocks):
-            node = np.argmax(self.effective)
-            # Main formula for effectiveness
-            self.available[node] -= self.block_size
-            # No more available storage!
-            if (self.available[node] < 0):
-                # Roll back the change
-                self.available[node] += self.block_size
-                return None
-            nodes.append(np.asscalar(node))
-            self.effective[node] = ((self.available[node]/self.capacities[node])**2)/self.latencies[node]
+    def placement_node_ids(self):
+        node = np.argmax(self.effective)
+        # Main formula for effectiveness.
+        self.available[node] -= self.block_size
+        # No more available storage!
+        if (self.available[node] < 0):
+            # Roll back the change
+            self.available[node] += self.block_size
+            return None
+        # update the effectiveness of the node.
+        self.effective[node] = (
+            (self.available[node]/self.capacities[node])**2)/self.latencies[node]
 
-        return nodes
+        return np.asscalar(node)
 
     def delete_file(self, filename):
         if filename not in self.metadata:
             return None
 
         command = "rm -rf {}".format(self.config['path'] + filename)
-        nodes = self.metadata[filename]['nodes']
+        node = self.metadata[filename]['node']
         del self.metadata[filename]
-
         self.persist_metadata()
-        self.execute_on_nodes(nodes, self.execute_command, args=(command,))
-        return 1.0
+        print("delete_file: ", command, node)
+        return self.execute_command(node, command)
 
     def retrieve_file(self, filename, output='./'):
         if filename not in self.metadata:
             return None
         else:
-            nodes = self.metadata[filename]['nodes']
+            node = self.metadata[filename]['node']
             self.metadata[filename]['modified'] = time.strftime("%a, %d %b %Y %H:%M:%S GMT", time.gmtime())
-            self.execute_on_nodes(nodes, self.execute_scp, args=(filename, output))
-        return 1.0
+        return run(['scp', node + ':' + self.config['path'] + filename, output])
 
     def list_files(self):
         return self.metadata
